cfd59fea5dace1558bcfbd8e0948d2521ca55566
sayja563 <sayja563@gmail.com>
2023-02-19 18:10:09 +0700

    Bo sung them

--- ThucHanh2/SDPhanSo.java
+++ ThucHanh2/SDPhanSo.java
@@ -55,23 +55,23 @@ public class SDPhanSo {
         System.out.print("Gia tri cua phan so b la: ");	
 		b.hienThi();
 
-        // PhanSo x = new PhanSo();
-        // PhanSo y = new PhanSo();
-        // System.out.println();
-        // System.out.println("Nhap gia tri cho phan so x: ");	
-        // x.nhapPhanSo();
-        // System.out.println("Nhap gia tri cho phan so y: ");	
-        // y.nhapPhanSo();
+        PhanSo x = new PhanSo();
+        PhanSo y = new PhanSo();
+        System.out.println();
+        System.out.println("Nhap gia tri cho phan so x: ");	
+        x.nhapPhanSo();
+        System.out.println("Nhap gia tri cho phan so y: ");	
+        y.nhapPhanSo();
 
-        // PhanSo nghich_dao_x = x.giaTriNghichDao();
-        // System.out.println();
-        // System.out.print("Gia tri nghich dao cua phan so x la: ");	
-	    // nghich_dao_x.hienThi();
+        PhanSo nghich_dao_x = x.giaTriNghichDao();
+        System.out.println();
+        System.out.print("Gia tri nghich dao cua phan so x la: ");	
+	    nghich_dao_x.hienThi();
 
-        // PhanSo tong = x.congPhanSo(y);
-        // System.out.println();
-        // System.out.print("Tong cua x va y la: ");	
-	    // tong.hienThi();
+        PhanSo tong = x.congPhanSo(y);
+        System.out.println();
+        System.out.print("Tong cua x va y la: ");	
+	    tong.hienThi();
 
         System.out.println("\n");
         System.out.print("Nhap so luong phan tu cua mang phan so: ");	
