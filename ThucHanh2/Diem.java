03d77791bfb76a06d8a28bd929e0cf34fc46ffe6
sayja563 <96453805+sayja563@users.noreply.github.com>
2023-02-19 09:04:04 +0700

    Add files via upload

---
+++ ThucHanh2/Diem.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class Diem {
+ 	private int x, y;
+	
+	
+	public Diem()
+	{
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public Diem(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	public void nhapDiem()
+	{
+		Scanner sc = new Scanner(System.in);
+		String input;
+		
+		System.out.print("Nhap x: ");
+		input = sc.nextLine();		
+		try
+		{
+			this.x = Integer.parseInt(input);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Loi nhap lieu.");
+		}
+		
+		System.out.print("Nhap y: ");
+		input = sc.nextLine();
+		try
+		{
+			this.y = Integer.parseInt(input);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Loi nhap lieu.");
+		}
+	}
+	
+	public void hienThi()
+	{
+		System.out.println("(" + this.x + ", " + this.y + ")");
+	}
+	
+	public int giaTriX()
+	{
+		return this.x;
+	}
+	
+	public int giaTriY()
+	{
+		return this.y;
+	}
+	
+	
+	public void doiDiem(int dX, int dY)
+	{
+		this.x += dX;
+		this.y += dY;
+	}
+	
+	public float khoangCach()
+	{
+		double khoang_cach = 0;
+		khoang_cach = Math.sqrt(this.x * this.x + this.y * this.y);
+		
+		return (float)khoang_cach;
+	}
+	
+	public float khoangCach(Diem d)
+	{
+		double khoang_cach = 0;
+		khoang_cach = Math.sqrt((this.x - d.x) * (this.x - d.x) + (this.y - d.y) * (this.y - d.y));
+		
+		return (float)khoang_cach;
+	}
+
+}
---
+++ ThucHanh2/PhanSo.java
@@ -0,0 +1,150 @@
+import java.util.Scanner;
+
+public class PhanSo {
+	private int tu_so, mau_so;
+	
+	public PhanSo()
+	{
+		this.tu_so = 0;
+		this.mau_so = 1;
+	}
+	
+	public PhanSo(int tu_so, int mau_so){
+		this.tu_so = tu_so;
+		this.mau_so = mau_so;
+	}
+	
+	public void hienThi()
+	{
+		if (this.tu_so == 0)
+		{
+			System.out.print("0");
+		}
+		else if (this.mau_so == 1)
+		{
+			System.out.print(this.tu_so);
+		}
+		else
+		{
+			System.out.print(this.tu_so + "/" + this.mau_so);
+		}
+	}
+	
+	public int getValue()
+	{
+		Scanner sc = new Scanner(System.in);
+		String input;
+		
+		
+		int value = 0;
+		try {
+			input = sc.nextLine();
+			value = Integer.parseInt(input);
+		}
+		catch (Exception e)
+		{
+			System.out.print("Loi nhap lieu. Moi nhap lai: ");
+		}		
+		
+		return value;
+	}
+	
+	public boolean hopLe()
+	{
+		if (this.mau_so == 0)
+			return false;
+		
+		return true;
+	}
+	
+	public void nhapPhanSo()
+	{
+		do
+		{
+			System.out.print("Nhap tu so: ");
+			this.tu_so = getValue();
+			System.out.print("Nhap mau so: ");
+			this.mau_so = getValue();
+			
+			if (!this.hopLe())
+			{
+				System.out.println("Phan so khong hop le. Moi nhap lai.");
+			}
+		}
+		while (!this.hopLe());
+	}
+	
+	public float giaTriThuc()
+	{
+		return (float)this.tu_so/this.mau_so;
+	}
+	
+	public boolean lonHon(PhanSo a)
+	{
+		return (this.tu_so * a.mau_so) > (this.mau_so * a.tu_so);
+	}
+
+	public int uocChungLonNhat(int a, int b)
+	{		
+		int ucln = 0;
+		while(ucln != 0)
+		{	
+			if (a % b == 0)
+				ucln = b;
+			if (b % a == 0)
+				ucln = a;
+			
+			if (a > b)
+			{
+				b = a % b;
+				a /= b;
+			}
+			if (a < b)
+			{
+				a = b % a;
+				b /= a;
+			}
+		}
+
+		return ucln;
+	}
+
+	public void rutGonPhanSo()
+	{
+		int ucln = uocChungLonNhat(this.tu_so, this.mau_so);
+		this.tu_so /= ucln;
+		this.mau_so /= ucln;
+	}
+
+	// public PhanSo congPhanSo(PhanSo x)
+	// {
+
+	// }
+
+	public static void main(String[] args) {
+		PhanSo a = new PhanSo();
+		a.nhapPhanSo();
+		System.out.println("Gia tri thuc cua phan so la: " + a.giaTriThuc());		
+		a.rutGonPhanSo();
+		System.out.println("Gia tri thuc cua phan so sau khi rut gon la: ");	
+		a.hienThi();
+
+		// PhanSo b = new PhanSo(4, 6);
+		
+		// if (a.lonHon(b))
+		// {
+		// 	System.out.print("Phan so ");
+		// 	a.hienThi();
+		// 	System.out.print(" lon hon phan so ");
+		// 	b.hienThi();
+		// }
+		// else
+		// {
+		// 	System.out.print("Phan so ");
+		// 	a.hienThi();
+		// 	System.out.print(" nho hon phan so ");
+		// 	b.hienThi();
+		// }
+	}
+
+}
